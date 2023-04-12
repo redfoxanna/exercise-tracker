b1d02b59175e13a25e5525039cffa93970df91ff
Anna Kessler <akessler@madisoncollege.edu>
2023-04-12 16:30:20 -0500

    fix the profile things.

--- exercisetrackerfileconstants.php
+++ exercisetrackerfileconstants.php
@@ -2,4 +2,4 @@
 // Exercise Tracker File Constants
 define('ET_UPLOAD_PATH', 'images/');
 define('ET_MAX_FILE_SIZE', 524288);
-define('ET_DEFAULT_PROFILE_FILE_NAME', 'generic_profile.png');
+define('ET_DEFAULT_PROFILE_FILE_NAME', 'images/generic_profile.png');
